45eb7daf3cb64945a12fa92632f44cb85672a8f2
Markus <mark113c@stud.kea.dk>
2021-09-09 00:07:09 +0200

    Making it look pretty

--- dist/index.js
+++ dist/index.js
@@ -117,38 +117,74 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"script.js":[function(require,module,exports) {
-"use strict";
+})({"../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
 
-window.addEventListener("DOMContentLoaded", start);
-
-function one(query) {
-  return document.querySelector(query);
-}
-
-function start() {
-  document.addEventListener("scroll", changeLogo);
-}
-
-function changeLogo(e) {
-  //   console.log(document.querySelector(".cta").scrollTop);
-  if (window.scrollY > 300) {
-    one(".logo").classList.remove("hide");
-    one(".logo").classList.add("show");
-    one(".cta").classList.remove("hide");
-    one(".cta").classList.add("show");
-    one(".splash-logo").classList.add("hide");
-    one(".splash-logo").classList.remove("show");
-  } else {
-    one(".logo").classList.add("hide");
-    one(".logo").classList.remove("show");
-    one(".cta").classList.add("hide");
-    one(".cta").classList.remove("show");
-    one(".splash-logo").classList.remove("hide");
-    one(".splash-logo").classList.add("show");
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
   }
+
+  return bundleURL;
 }
-},{}],"../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -352,5 +388,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","script.js"], null)
-//# sourceMappingURL=/script.75da7f30.js.map
+},{}]},{},["../../../../../../.npm-global/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/index.js.map
--- script.js
+++ script.js
@@ -11,11 +11,24 @@ function start() {
 }
 
 function changeLogo(e) {
-  if (window.scrollY > 500) {
+  //   console.log(document.querySelector(".cta").scrollTop);
+  if (window.scrollY > 300) {
     one(".logo").classList.remove("hide");
     one(".logo").classList.add("show");
 
+    one(".cta").classList.remove("hide");
+    one(".cta").classList.add("show");
+
     one(".splash-logo").classList.add("hide");
     one(".splash-logo").classList.remove("show");
+  } else {
+    one(".logo").classList.add("hide");
+    one(".logo").classList.remove("show");
+
+    one(".cta").classList.add("hide");
+    one(".cta").classList.remove("show");
+
+    one(".splash-logo").classList.remove("hide");
+    one(".splash-logo").classList.add("show");
   }
 }
